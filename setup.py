3e9b4309f6920ae3d89e2434086ba070b36baa02
Alexandra Paxton <paxton.alexandra@gmail.com>
2020-02-04 18:10:49 -0500

    Updating release and copyright

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ DISTNAME = 'align'
 DESCRIPTION = 'Analyzing Linguistic Interaction with Generalizable techNiques'
 MAINTAINER = 'N. Duran, A. Paxton, & R. Fusaroli'
 MAINTAINER_EMAIL = 'paxton.alexandra@gmail.com'
-VERSION = '0.0.9'
+VERSION = '0.0.10'
 
 # Specify minimum versions for scipy and numpy
 SCIPY_MIN_VERSION = '1.3.1'
